0e0dee0e12295b7882fa6b31422461f33a9b4588
MarlenKoch <marlen.koch@sap.com>
2025-02-27 16:39:29 +0100

    change heading

--- frontend/src/components/UeberUns.tsx
+++ frontend/src/components/UeberUns.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const UeberUns: React.FC = () => {
   return (
     <div>
-      <h2>Text...</h2>
+      <h1>Entwicklung einer Website mithilfe des Frameworks „React“</h1>
       <p>Wissenschaftilicher text hierhin pasten</p>
       {/* Add more data safety details here */}
     </div>
