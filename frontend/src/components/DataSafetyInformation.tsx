7eaf67027a17c3c200b055205de77766c89c9a28
MarlenKoch <marlen.koch@sap.com>
2025-02-27 16:22:00 +0100

    Datenschutz fertig

--- frontend/src/components/DataSafetyInformation.tsx
+++ frontend/src/components/DataSafetyInformation.tsx
@@ -3,9 +3,10 @@ import React from "react";
 const DataSafetyInformation: React.FC = () => {
   return (
     <div>
-      <h2>Data Safety Information</h2>
-      <p>This is the Data Safety Information page.</p>
-      {/* Add more data safety details here */}
+      <h1>Datenschutzhinweis</h1>
+      <p>Unser Projekt Crümbli wird nur für Studienzwecke verwendet und ist nicht öffentlich zugänglich. Daher gibt es kein Risiko für die Daten unserer Nutzer:innen, da keine Drittparteien auf diese zugreifen können.</p>
+      <p>Bitte beachten Sie, dass trotz all unserer Bemühungen zum Schutz Ihrer Daten keinerlei Schutzmaßnahmen gegen unvorhergesehene intergalaktische Ereignisse existieren. Sollten Aliens unerwartet mitten in der Nacht unser Projekt hacken, übernehmen wir dafür keine Haftung.</p>
+      <p>Wir versichern, dass alle uns anvertrauten Daten vertraulich behandelt und nicht außerhalb des studentischen Projektkontexts genutzt werden.</p>
     </div>
   );
 };
